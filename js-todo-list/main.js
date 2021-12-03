334700d8bb9eea9f03c8764d2d7e98006c92f8b9
roywangroytw <roywangroy.tw@gmail.com>
2021-12-03 11:30:50 +0800

    finish task 1: switch class checked item

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,18 @@
 // TO DO
+document.addEventListener("DOMContentLoaded", () => {
+  const toDoItem = document.querySelectorAll("li");
+  const toDoItemBox = document.querySelector("ul");
+
+  // Task 1
+  toDoItemBox.addEventListener("click", (e) => {
+    if (
+      e.target &&
+      e.target.nodeName === "LI" &&
+      e.target.className === "checked"
+    ) {
+      e.target.classList.remove("checked");
+    } else {
+      e.target.classList.add("checked");
+    }
+  });
+});
